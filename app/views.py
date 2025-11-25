6928c332ca3f8d25c81e115c939cf16ba9652c8d
ddderrr <fewez1349@gmail.com>
2025-11-25 19:19:39 +0700

    feat(frontend): add jQuery function to display JSON data

--- app/views.py
+++ app/views.py
@@ -1,6 +1,23 @@
+from flask import jsonify
 from app import app
 
 
 @app.route('/')
 def home():
     return "Rattananarin says 'Hello World!'"
+
+
+@app.route('/phonebook')
+def index():
+    return app.send_static_file('phonebook.html')
+
+
+# This route serves the dictionary d at the route /data
+@app.route("/api/data")
+def data():
+    # define some data
+    d = {
+        "Alice": "(708) 727-2377",
+        "Rattananarin": "06-4002-2307"
+    }
+    return jsonify(d)  # convert your data to JSON and return
