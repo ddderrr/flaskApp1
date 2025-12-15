726d845fbdce94fcd7557ed28770e903b0eea5fc
ddderrr <fewez1349@gmail.com>
2025-12-15 17:26:25 +0700

    Lab03

--- app/views.py
+++ app/views.py
@@ -1,3 +1,35 @@
-from flask import jsonify
-from app import app
+# This route serves the dictionary d at the route /api/data
+import datetime
+from flask import jsonify, render_template
+from app import app 
 from app import hw_views
+
+
+@app.route("/")
+def home():
+    return "Flask says 'Hello world!'"
+
+
+@app.route('/crash')
+def crash():
+    return 1/0
+
+
+@app.route('/lab03')
+def lab03_home():
+    return render_template('lab03/index.html',
+                           utc_dt=datetime.datetime.utcnow())
+
+@app.route('/lab03/about/')
+def lab03_about():
+    return render_template('lab03/about.html')
+
+@app.route('/lab03/comments/')
+def lab03_comments():
+    comments = ['This is the first comment.',
+                'This is the second comment.',
+                'This is the third comment.',
+                'This is the fourth comment.']
+
+
+    return render_template('lab03/comments.html', comments=comments)
